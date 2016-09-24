936597b80f2c0fac75d3c85e541ad8606a21abcd
Ben <benhunt29@gmail.com>
2016-09-24 16:01:12 -0500

    test out installing from npm instead of bower

--- chartist-plus.js
+++ chartist-plus.js
@@ -1,12 +1,14 @@
-const Chartist = require('./bower_components/chartist/dist/chartist.min.js');
-const chartistPluginAxisTitle = require('./bower_components/chartist-plugin-axistitle/dist/chartist-plugin-axistitle.min.js');
-const chartistPluginTooltip = require('./bower_components/chartist-plugin-tooltip/dist/chartist-plugin-tooltip.min.js');
-const chartistPluginZoom = require('./bower_components/chartist-plugin-zoom/dist/chartist-plugin-zoom.min.js');
+// const Chartist = require('./bower_components/chartist/dist/chartist.min.js');
+// const chartistPluginAxisTitle = require('./bower_components/chartist-plugin-axistitle/dist/chartist-plugin-axistitle.min.js');
+// const chartistPluginTooltip = require('./bower_components/chartist-plugin-tooltip/dist/chartist-plugin-tooltip.min.js');
+// const chartistPluginZoom = require('./bower_components/chartist-plugin-zoom/dist/chartist-plugin-zoom.min.js');
 
+const Chartist = require('chartist');
+const chartistAxisTitle = require('chartist-plugin-axistitle');
 var ChartistPlus = {
     BarChart: function (selector, data, options = {}, responsiveOptions, pluginOptions) {
         options.plugins = options.plugins || [];
-            options.plugins.push(Chartist.plugins.ctAxisTitle({
+            options.plugins.push(chartistAxisTitle({
                 axisX: {
                     axisTitle: data.xAxisLabel || '',
                     axisClass: 'ct-axis-title',
