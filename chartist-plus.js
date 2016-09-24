19a4d258bb9d7e322b1efee7164ea4fe2b754032
Ben <benhunt29@gmail.com>
2016-09-24 15:20:21 -0500

    start filling out main module

--- chartist-plus.js
+++ chartist-plus.js
@@ -1,1 +1,35 @@
-const Chartist = require('./bower_components/chartist/dist/chartist.js');
+const Chartist = require('./bower_components/chartist/dist/chartist.min.js');
+const chartistPluginAxisTitle = require('./bower_components/chartist-plugin-axistitle/dist/chartist-plugin-axistitle.min.js');
+const chartistPluginTooltip = require('./bower_components/chartist-plugin-tooltip/dist/chartist-plugin-tooltip.min.js');
+const chartistPluginZoom = require('./bower_components/chartist-plugin-zoom/dist/chartist-plugin-zoom.min.js');
+
+var ChartistPlus = {
+    BarChart: function (selector, data, options = {}, responsiveOptions, pluginOptions) {
+        options.plugins = options.plugins || [];
+            options.plugins.push(Chartist.plugins.ctAxisTitle({
+                axisX: {
+                    axisTitle: data.xAxisLabel || '',
+                    axisClass: 'ct-axis-title',
+                    offset: {
+                        x: 0,
+                        y: 30
+                    },
+                    textAnchor: 'middle'
+                },
+                axisY: {
+                    axisTitle: data.yAxisLabel || '',
+                    axisClass: 'ct-axis-title',
+                    offset: {
+                        x: 0,
+                        y: -5
+                    },
+                    textAnchor: 'middle',
+                    flipTitle: false
+                }
+            }));
+
+        return new Chartist.Bar(selector, data, options)
+    }
+};
+
+module.exports = ChartistPlus;
