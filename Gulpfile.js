2f968b42a7a84c38113762a13d2d58c773b5b346
bhunt <bhunt@techtrav.com>
2016-10-12 10:12:42 -0500

    pull in tooltip css

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,6 +2,7 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var concatCss = require('gulp-concat-css');
 
 var sassOptions = {
     errLogToConsole: true,
@@ -9,8 +10,16 @@ var sassOptions = {
     includePaths: 'node_modules/chartist/dist/scss'
 };
 
-gulp.task('default', function() {
+gulp.task('default', ['sass', 'css']);
+
+gulp.task('sass', function() {
     return gulp.src('./scss/**/*.scss')
         .pipe(sass(sassOptions))
         .pipe(gulp.dest('.'))
+});
+
+gulp.task('css', ['sass'], function() {
+   return gulp.src(['node_modules/chartist-plugin-tooltips/dist/chartist-plugin-tooltip.css', 'chartist-plus.css'])
+       .pipe(concatCss('chartist-plus.css'))
+       .pipe(gulp.dest('.', {overwrite: true}))
 });
